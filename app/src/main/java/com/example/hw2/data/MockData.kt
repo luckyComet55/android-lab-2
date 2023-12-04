259b8f40bca64b17d13f99c48c9f1c5ca6913dd2
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-12-04 17:05:22 +0300

    Revert "Refactor data class"
    This reverts commit 384399ee1f1944891fcdebaf903a5a39e74319e9.

--- app/src/main/java/com/example/hw2/data/MockData.kt
+++ app/src/main/java/com/example/hw2/data/MockData.kt
@@ -4,22 +4,27 @@ import com.example.hw2.domain.Note
 
 val mockNotesData = listOf<Note>(
     Note(
+        "Check engine",
         "Check engine in the car, it's been sounding strange later",
         1
     ),
     Note(
-        "smth",
+        "Buy groceries",
+        "",
         2
     ),
     Note(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper varius justo, at sagittis odio interdum sed. Proin et tellus vitae tortor semper mattis eu at mi. Ut eu purus odio. Etiam pharetra felis vitae arcu elementum, et fermentum nulla sagittis. Nunc congue, nulla id maximus lobortis, eros neque gravida dui, ut iaculis ipsum nulla sit amet lectus. Curabitur ac sem vitae dui hendrerit finibus. Pellentesque eu viverra orci. Vestibulum lacinia ut mauris ac ultrices. Aenean porta ultricies nulla ac pulvinar. Donec semper quis turpis et dictum. Nulla accumsan ac erat nec tincidunt. Nam porta magna sit amet eros facilisis, sit amet aliquam massa malesuada. Donec scelerisque congue nunc vel varius. Nunc aliquam libero a est facilisis aliquam id eget sapien.",
+        "Very very very very very very very very very very very very very very long title",
+        "Testing how it works",
         3
     ),
     Note(
+        "s",
         "Now checking how reallly short title looks",
         4
     ),
     Note(
+        "Truth is like poetry",
         "And everybody hecking hates poetry",
         5
     )
