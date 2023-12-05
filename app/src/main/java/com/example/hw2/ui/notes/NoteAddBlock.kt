e25cba12931c9686dced440203085384b31fab73
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-12-05 12:21:40 +0300

    Finishing touches on UI

--- app/src/main/java/com/example/hw2/ui/notes/NoteAddBlock.kt
+++ app/src/main/java/com/example/hw2/ui/notes/NoteAddBlock.kt
@@ -1,9 +1,12 @@
 package com.example.hw2.ui.notes
 
+import androidx.compose.foundation.layout.Arrangement
 import com.example.hw2.R
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
@@ -29,7 +33,11 @@ fun NoteAddBlock(
     var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(""))
     }
-    Row(modifier = modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         TextField(
             value = text,
             onValueChange = {
@@ -37,10 +45,15 @@ fun NoteAddBlock(
             },
             placeholder = {
                 Text(text = stringResource(id = R.string.editor_hint))
-            }
+            },
+            singleLine = true,
+            modifier = Modifier.weight(4f)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = onAddButtonClick) {
+        Button(
+            onClick = onAddButtonClick,
+            modifier = Modifier.weight(1f)
+        ) {
             Text(text = stringResource(id = R.string.add_note_btn))
         }
     }
